bca8c67fe5cedbe331a1ef3251d40b188a75482b
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-08-21 16:50:24 +0200

    bonus done

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import AppTitle from './components/AppTitle.vue'
+import jumbo from './components/Jumbo.vue'
 
 export default {
   components: {
     AppTitle,
+    jumbo
   }
 }
 </script>
 
 <template>
   <AppTitle></AppTitle>
+  <jumbo></jumbo>
 </template>
 
---
+++ src/components/Jumbo.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'jumbo',
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="img-container">
+            <img src="../assets/01.webp" alt="jumbo img morales">
+        </div>
+        <p>non è per niente facile questa roba</p>
+    </div>
+</template>
+
+<style>
+.container {
+    display: flex;
+    justify-content: center;
+}
+.img-container {
+    height: 500px;
+    width: 600px;
+}
+
+.img-container img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+p {
+    position: absolute;
+    font-size: .7rem;
+}
+</style>
